e3a2e763a75079bdee692d7e2fc86689f7097a06
Alan <alanbritodossantos2015@gmail.com>
2022-06-11 21:41:37 -0300

    implementado a listagem de tarefas p1

--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,15 +1,19 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ListarTarefaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule
   ],
   providers: [
     TarefaService
